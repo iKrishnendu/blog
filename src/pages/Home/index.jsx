7322632e13da4606e840d2e80cfc23fe0a266602
Krishnendu Sahoo <102577456+iKrishnendu@users.noreply.github.com>
2023-04-19 23:59:25 +0530

    commit

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,14 +2,45 @@ import React from "react";
 import Header from "../../components/Home/Header";
 import SearchBar from "../../components/Home/SearchBar";
 import BlogList from "../../components/Home/BlogList";
+import EmptyList from "../../components/common/EmptyList";
 import { blogList } from "../../components/config/data";
 
 const Home = () => {
+  const [blogs, setBlogs] = useState(blogList);
+  const [searchKey, setSearchKey] = useState("");
+
+  // Search submit
+  const handleSearchBar = (e) => {
+    e.preventDefault();
+    handleSearchResults();
+  };
+
+  // Search for blog by category
+  const handleSearchResults = () => {
+    const allBlogs = blogList;
+    const filteredBlogs = allBlogs.filter((blog) =>
+      blog.category.toLowerCase().includes(searchKey.toLowerCase().trim())
+    );
+    setBlogs(filteredBlogs);
+  };
+
+  // Clear search and show all blogs
+  const handleClearSearch = () => {
+    setBlogs(blogList);
+    setSearchKey("");
+  };
+
   return (
     <div>
       <Header />
-      <SearchBar />
-      <BlogList blogs={blogList} />
+      <SearchBar
+        value={searchKey}
+        clearSearch={handleClearSearch}
+        formSubmit={handleSearchBar}
+        handleSearchKey={(e) => setSearchKey(e.target.value)}
+      />
+      {/* Blog List & Empty View */}
+      {!blogs.length ? <EmptyList /> : <BlogList blogs={blogs} />}
     </div>
   );
 };
